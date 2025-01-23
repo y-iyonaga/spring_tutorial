a99cc1dac95583b206e8faf977d7be8a3021d2e4
y-iyonaga <job.iyonaga@gmail.com>
2025-01-23 10:33:44 +0900

    追加 トップページの表示

---
+++ src/main/java/com/example/its/web/IndexController.java
@@ -0,0 +1,15 @@
+package com.example.its.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller // Springにコントローラークラスだと伝える
+public class IndexController {
+
+    @GetMapping("/") //getのトップページに紐づけてる
+//    @ResponseBody // 戻り値がそのままレスポンスになる　←retuneを直書きではなくファイル指定にしたためいらなくなった
+    public String index() {
+        return "index"; //index.htmlのこと。拡張子は書かなくてよい、テクノロジーに依存しなくて済む。
+    }
+}
