d04badc3025fe5dfb93882317c1fe4db670ce377
y-iyonaga <job.iyonaga@gmail.com>
2025-01-24 10:03:35 +0900

    追加 入力フォームの作成

--- src/main/java/com/example/its/web/issue/IssueController.java
+++ src/main/java/com/example/its/web/issue/IssueController.java
@@ -6,19 +6,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/issues") // これのおかげで@GetMappingのパスを省略
 @RequiredArgsConstructor
 public class IssueController {
 
     private final IssueService issueService;
 
-    @GetMapping("/issues")
+    @GetMapping
     public String showList(Model model) {
         model.addAttribute("issueList", issueService.findAll()); // IssueService.javaからfindAllを受け取る
         return "issues/List"; // テンプレートフォルダから見てissuesのディレクトリ内にあるList.htmlをさしている
+    }
+
+    // GET /issues/creationForm  ←このリクエストが来た時用
+    @GetMapping("/creationForm")
+    public String showCreationForm() {
+        return "issues/creationForm";
     }
 }
 
