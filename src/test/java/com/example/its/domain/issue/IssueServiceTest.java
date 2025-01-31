8eb18913566369654d39b7563aaad23869bee8b0
y-iyonaga <job.iyonaga@gmail.com>
2025-01-31 14:17:56 +0900

    追加 serviceテストコードに補足追加

--- src/test/java/com/example/its/domain/issue/IssueServiceTest.java
+++ src/test/java/com/example/its/domain/issue/IssueServiceTest.java
@@ -1,8 +1,9 @@
 package com.example.its.domain.issue;
 
-import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -21,22 +22,23 @@ class IssueServiceTest {
     private IssueService issueService;
 
     @Test
-    // 課題一覧を取得できる
-    void 課題一覧を取得できる() {
+    @DisplayName("課題一覧が取得できることを確認")
+    void testFindAllIssues() {
         // モックデータ
         List<IssueEntity> mockIssues = List.of(
                 new IssueEntity(1L, "バグA", "バグがあります"),
                 new IssueEntity(2L, "機能要望B", "Bに追加機能が欲しいです")
         );
         when(issueRepository.findAll()).thenReturn(mockIssues);
-
+        // ↑ when(モックオブジェクト.メソッド()).thenReturn(戻り値);
         // メソッド実行
         List<IssueEntity> result = issueService.findAll();
 
-        // 検証
-        assertThat(result).hasSize(2)
-                .extracting(IssueEntity::getSummary)
-                .containsExactly("バグA", "機能要望B");
+        SoftAssertions softly = new SoftAssertions(); // SoftAssertions を使うとすべてを評価してから結果をまとめて報告できる
+        softly.assertThat(result).hasSize(2); // リストのサイズ
+        softly.assertThat(result).extracting(IssueEntity::getId) // 1Lと2Lが順番通りか（リスト内の順番）
+                .containsExactly(1L, 2L);
+        softly.assertAll(); // すべてのアサーションを評価した後、一度にエラーメッセージを表示する
 
         // モックメソッドの呼び出し回数を確認
         verify(issueRepository, times(1)).findAll();
